037b48e57142c8d06059ea7d7afa729c19464a0b
Darshil Jain <pihujain9898@gmail.com>
2024-09-08 14:16:32 +0530

    Seller middleware updated

--- src/middlewares/sellerIsApproved.js
+++ src/middlewares/sellerIsApproved.js
@@ -6,11 +6,12 @@ const  sellerService  = require('../services/seller.service');
 const sellerIsApproved = async(req, res, next) => {
     const seller = await sellerService.getSellerByUserId(req.user._id);
     if (!seller) 
-      return res.send(new ApiError(httpStatus.NOT_FOUND, 'Seller not found'));
-    if (!seller.isApproved) 
-      return res.send(new ApiError(httpStatus.NOT_FOUND, 'Seller not approved'));
-    next(); 
+      return res.send(new ApiError(httpStatus.NOT_FOUND, 'Seller not created profile'));
 
+    if (seller.isApproved == "approved") 
+      next(); 
+    else
+      return res.send(new ApiError(httpStatus.NOT_FOUND, 'Seller not approved'));
 };
 
 
